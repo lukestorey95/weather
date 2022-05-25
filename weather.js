f45476335be18260ad973bc3cbbf3ffe5fd914d6
Luke Storey <lukemstorey@gmail.com>
2022-05-25 17:47:07 +0100

    Add displayWeather function

--- weather.js
+++ weather.js
@@ -1,19 +1,22 @@
- class Weather {
-
+class Weather {
   constructor(api) {
     this.weatherAPI = api;
   }
 
   fetch(city) {
     this.weatherAPI.fetchWeatherData(
-    city, 
-    result => this.weatherData = result
-    )
+      city,
+      (result) => (this.weatherData = result)
+    );
   }
 
   getWeatherData() {
     return this.weatherData;
-  } 
- }
+  }
 
- module.exports = Weather
+  displayWeather() {
+    return `City: ${this.weatherData.name}\nWeather: ${this.weatherData.weather[0].main}\nTemperature: ${this.weatherData.main.temp}\nFeels like: ${this.weatherData.main.feels_like}\nHumidity: ${this.weatherData.main.humidity}`;
+  }
+}
+
+module.exports = Weather;
--- weather.test.js
+++ weather.test.js
@@ -1,23 +1,42 @@
 // const WeatherApi = require("./weatherApi");
-const Weather = require('./weather')
+const Weather = require("./weather");
 
-describe('Weather', () => {
-  it('returns the weather of a given city', () => {
+describe("Weather", () => {
+  it("returns the weather of a given city", () => {
     const mockAPI = {
       fetchWeatherData: (city, callback) => {
         callback({
-          name: 'Bristol',
-          weather: '40 degrees'
-        })
-      }
+          name: "Bristol",
+          weather: "40 degrees",
+        });
+      },
     };
 
     weather = new Weather(mockAPI);
-    weather.fetch('Bristol');
+    weather.fetch("Bristol");
 
     expect(weather.getWeatherData()).toEqual({
-      name: 'Bristol',
-      weather: '40 degrees'
-    })
-  })
+      name: "Bristol",
+      weather: "40 degrees",
+    });
+  });
+
+  it("formats the weather in a pretty breakdown", () => {
+    const mockAPI = {
+      fetchWeatherData: (city, callback) => {
+        callback({
+          name: "London",
+          weather: [{ main: "Clouds" }],
+          main: { temp: "18.4", feels_like: "16.0", humidity: "64%" },
+        });
+      },
+    };
+
+    weather = new Weather(mockAPI);
+    weather.fetch("London");
+
+    expect(weather.displayWeather()).toEqual(
+      "City: London\nWeather: Clouds\nTemperature: 18.4\nFeels like: 16.0\nHumidity: 64%"
+    );
+  });
 });
